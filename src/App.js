2e3a7a09a49d3e34af1d79fd6e47d25686126696
Yohannes Welday <yohanneswelday777@gmail.com>
2023-09-04 03:30:16 +0300

    Setting up react project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-// import './App.css';
-import Header from './components/header';
-import Footer from './components/footer';
-import Main from './components/main';
+import React from "react";
+import "boxicons"
 
+
+import { Route, Routes } from "react-router-dom";
+
+import './App.css';
+import Header from "./components/Header";
+import Landing from "./components/Landing";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Header/>
+      <box-icon name="right-arrow"></box-icon>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="/projects" element = {<Projects/>}/>
+        <Route path="/contact-me" element={<Contact/>}/>
+
+      </Routes>
+  
     </div>
   );
 }
